bfe09396ddbe1bed6117e07cd435cbcd3a61b42a
RVany <vanmathyrr@gmail.com>
2023-07-18 10:37:53 -0400

    Initial commit

---
+++ src/webparts/spfxTeamsSpecific/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ISpfxTeamsSpecificWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'SpfxTeamsSpecificWebPartStrings' {
+  const strings: ISpfxTeamsSpecificWebPartStrings;
+  export = strings;
+}
